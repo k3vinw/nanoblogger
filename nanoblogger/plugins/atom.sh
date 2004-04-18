c87cfc3c317a403f71adf2d518f6133f2a65355b
n1xt3r <n1xt3r@2a92b892-5743-0410-993c-ec182001eea1>
2004-04-18 03:46:01 +0000

    - NB_AtomEntryModDate was temporarily changed to NB_AtomEntryDate. - Switched to make_placeholder to generate NB_Entries. - Deleted files left in PARTS_DIR directory.

--- nanoblogger/plugins/atom.sh
+++ nanoblogger/plugins/atom.sh
@@ -18,29 +18,26 @@ build_atomfeed(){
 	for entry in $ARCHIVE_LIST; do
 	        read_entry "$BLOG_DIR"/"$ARCHIVES"/"$entry"
 		NB_AtomEntryDate=`echo "$NB_EntryID$BLOG_TZD"`
-		NB_AtomEntryModDate=`find "$BLOG_DIR/$ARCHIVES/$entry" -printf "%TY-%Tm-%TdT%TH:%TM:%TS$BLOG_TZD"`
-		load_template "$BLOG_DIR"/"$TEMPLATE_DIR"/"$template"
-		        if [ ! -z "$BLOG_HTML" ]; then
-			                echo "$BLOG_HTML" >> "$BLOG_DIR"/archives.tmp
-			                BLOG_HTML=
-			                PLACEHOLDER=
-			                NB_EntryCategories=
-			                NB_EntryCategoryTitle=
-		        fi
+		# non-portable find command!
+		#NB_AtomEntryModDate=`find "$BLOG_DIR/$ARCHIVES/$entry" -printf "%TY-%Tm-%TdT%TH:%TM:%TS$BLOG_TZD"`
+		NB_AtomEntryModDate="$NB_AtomEntryDate"
+		make_placeholder "$template" atom_entries.tmp "$output_file"
 	done
-	touch "$BLOG_DIR"/archives.tmp
-	cat "$BLOG_DIR"/archives.tmp > "$BLOG_DIR"/"$PARTS_DIR"/"$output_file"
-	rm -f "$BLOG_DIR"/archives.tmp
+	touch "$BLOG_DIR"/atom_entries.tmp
+	cat "$BLOG_DIR"/atom_entries.tmp > "$BLOG_DIR"/"$PARTS_DIR"/"$output_file"
+	rm -f "$BLOG_DIR"/atom_entries.tmp
 	}
 
 
 nb_msg "generating atom feed ..."
 build_atomfeed current nocat "$ATOMENTRY_TEMPLATE" atom.xml
+rm -f "$BLOG_DIR/$PARTS_DIR/atom.xml"
+NB_Entries="$PLACEHOLDER"
 
 # make atom feed (alternate to calling make_page)
 MKPAGE_OUTFILE="$BLOG_DIR/atom.xml"
-NB_Entries=`cat "$BLOG_DIR"/"$PARTS_DIR"/atom.xml`
 load_template "$ATOMFEED_TEMPLATE"
 echo "$BLOG_HTML" > "$MKPAGE_OUTFILE"
 nb_msg "$MKPAGE_OUTFILE"
 load_plugins plugins/postformat
+
