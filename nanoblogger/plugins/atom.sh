c47840245c45e676b6e69e0640b5e9b3c8f4990a
n1xt3r <n1xt3r@2a92b892-5743-0410-993c-ec182001eea1>
2004-05-19 06:23:31 +0000

    - Set NB_SYND_FILETYPE default to xml - Paul Drain <pd@cipherfunk.org>. - Updated rss and atom plugins to utilize NB_SYND_FILETYPE.

--- nanoblogger/plugins/atom.sh
+++ nanoblogger/plugins/atom.sh
@@ -31,12 +31,12 @@ build_atomfeed(){
 
 
 nb_msg "generating atom feed ..."
-build_atomfeed current nocat "$ATOMENTRY_TEMPLATE" atom.xml
-rm -f "$BLOG_DIR/$PARTS_DIR/atom.xml"
+build_atomfeed current nocat "$ATOMENTRY_TEMPLATE" atom."$NB_SYND_FILETYPE"
+rm -f "$BLOG_DIR/$PARTS_DIR/atom.$NB_SYND_FILETYPE"
 NB_Entries="$PLACEHOLDER"
 
 # make atom feed (alternate to calling make_page)
-MKPAGE_OUTFILE="$BLOG_DIR/atom.xml"
+MKPAGE_OUTFILE="$BLOG_DIR/atom.$NB_SYND_FILETYPE"
 load_template "$ATOMFEED_TEMPLATE"
 echo "$BLOG_HTML" > "$MKPAGE_OUTFILE"
 nb_msg "$MKPAGE_OUTFILE"
