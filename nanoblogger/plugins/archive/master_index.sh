4d94b119ac0cea18b98a361b9a0697636b00b9d0
n1xt3r <n1xt3r@2a92b892-5743-0410-993c-ec182001eea1>
2004-07-23 01:35:03 +0000

    - Added a couple new CSS style sheets, including a new default. - Entry's without title are now listed as "Untitled" in archives index page. - Category header and links only appear when categories exist in archives index page.

--- nanoblogger/plugins/archive/master_index.sh
+++ nanoblogger/plugins/archive/master_index.sh
@@ -13,6 +13,7 @@ if [ ! -z "$MOD_VAR" ] || [ "$weblog_update" = "all" ]; then
 	for entry in $ENTRY_LIST; do
 		month=`echo "$entry" |cut -c1-7`
 		read_entry "$NB_DATA_DIR/$entry"
+		[ -z "$NB_EntryTitle" ] && NB_EntryTitle="Untitled"
 		cat >> "$BLOG_DIR"/entry_links.tmp <<-EOF
 			<a href="$BLOG_URL/$MONTHLY_DIR/$month.$NB_FILETYPE">$month</a> - <a href="$NB_EntryPermalink">$NB_EntryTitle</a>
 			`[ ! -z "$NB_EntryCategories" ] && echo "- $NB_EntryCategories" |sed -e '{$ s/\,$//; }'`<br />
@@ -21,21 +22,32 @@ if [ ! -z "$MOD_VAR" ] || [ "$weblog_update" = "all" ]; then
 	NB_Entry_Links=`cat "$BLOG_DIR"/entry_links.tmp`
 	rm -f "$BLOG_DIR"/entry_links.tmp
 
-	# make NB_Archive_Links placeholder
-	cat > "$BLOG_DIR"/"$PARTS_DIR"/archive_links.$NB_FILETYPE <<-EOF
-		<a name="category"></a>
+	# make NB_Smart_CatLinks placeholder
+	cat > "$BLOG_DIR"/category_links.tmp <<-EOF
+		<a id="category" name="category"></a>
 		<b>Browse by category</b>
 		<div>
 		$NB_Category_Links
 		</div>
 		<br />
-		<a name="date"></a>
+	EOF
+	cat_total=`echo "$db_categories" |grep -c "[\.]db"`
+	if [ "$cat_total" -gt "0" ]; then
+		[ -f "$BLOG_DIR/category_links.tmp" ] &&
+			NB_Smart_CatLinks=`cat "$BLOG_DIR"/category_links.tmp`
+	fi
+	rm -f "$BLOG_DIR"/category_links.tmp
+
+	# make NB_Archive_Links placeholder
+	cat > "$BLOG_DIR"/"$PARTS_DIR"/archive_links.$NB_FILETYPE <<-EOF
+		$NB_Smart_CatLinks
+		<a id="date" name="date"></a>
 		<b>Browse by date</b>
 		<div>
 		$NB_Month_Links
 		</div>
 		<br />
-		<a name="entry"></a>
+		<a id="entry" name="entry"></a>
 		<b>Browse by entry</b>
 		<div>
 		$NB_Entry_Links
