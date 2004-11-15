4fe21c0754bc34a9cd0d95a407fa47f828aa63db
n1xt3r <n1xt3r@2a92b892-5743-0410-993c-ec182001eea1>
2004-11-15 07:52:17 +0000

    Brian Clayton <brian@servon.com>: Categories get sorted in a configurable manner.

--- nanoblogger/plugins/entry/category_links.sh
+++ nanoblogger/plugins/entry/category_links.sh
@@ -1,5 +1,8 @@
 # NanoBlogger Category Links Plugin
 # Entry Plugin to find related categories and generate links for them
+
+# Command to help filter order of categories
+: ${CATLINKS_FILTER_CMD:="sort"}
 query_db
 >"$SCRATCH_FILE".category_links
 get_cat_titles(){ for cat_db in $db_categories; do echo "`sed 1q $NB_DATA_DIR/$cat_db` | $cat_db"; done; }
@@ -9,8 +12,8 @@ for entry_catlinks in $CAT_TLIST; do
 	if [ ! -z "$cat_var" ]; then
 		cat_title=`sed 1q "$NB_DATA_DIR"/"$entry_catlinks"`
 		cat_index=`chg_suffix "$entry_catlinks"`; cat_feed=`chg_suffix "$entry_catlinks" "$NB_SYND_FILETYPE"`
-		sort >> "$SCRATCH_FILE".category_links <<-EOF
-			<a href="${ARCHIVES_PATH}$cat_index">$cat_title</a>,
+		$CATLINKS_FILTER_CMD >> "$SCRATCH_FILE".category_links <<-EOF
+			<!-- $cat_title --><a href="${ARCHIVES_PATH}$cat_index">$cat_title</a>,
 		EOF
 	fi
 done
