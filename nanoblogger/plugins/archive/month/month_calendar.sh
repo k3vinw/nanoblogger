2667abd95052d1de2b8cf24a7f9760d89c27d24b
n1xt3r <n1xt3r@2a92b892-5743-0410-993c-ec182001eea1>
2006-03-03 19:19:09 +0000

    - Patch #351596 from bugs.debian.org applied to fix calendar output. - Moved language definitions out to new 'lang' module.

--- nanoblogger/plugins/archive/month/month_calendar.sh
+++ nanoblogger/plugins/archive/month/month_calendar.sh
@@ -21,7 +21,7 @@ cal_month=`echo $month |cut -c6-7`
 CAL_HEAD=`echo "$CALENDAR" |sed -e '/^[ ]*/ s///g; 1q'`
 WEEK_DAYS=(`echo "$CALENDAR" |sed -n 2p`)
 DAYS=`echo "$CALENDAR" |sed 1,2d`
-NUM_DAY_LINES=(`echo "$DAYS" |grep -n "." |cut -d":" -f 1`)
+NUM_DAY_LINES=(`echo "$DAYS" |grep -n "[0-9]" |cut -d":" -f 1`)
 
 query_db "$cal_year.$cal_month"
 MONTH_LIST=(${DB_RESULTS[*]})
@@ -36,7 +36,7 @@ echo '</tr>' >> "$PLUGIN_OUTFILE"
 for line in ${NUM_DAY_LINES[@]}; do
 	DN_LINES=`echo "$DAYS" |sed -n "$line"p`
 	echo '<tr>' >> "$PLUGIN_OUTFILE"
-	echo "$DN_LINES" | sed -e '/  [ \t]/ s//<td style="text-align:center"><\/td>\ /g; /[0-9]/ s///g' >> "$PLUGIN_OUTFILE"
+	echo "$DN_LINES" | sed -e '/  $/ Q; /  [ \t]/ s//<td style="text-align: center;"><\/td>\ /g; /[0-9]/ s///g' >> "$PLUGIN_OUTFILE"
 	for dn in $DN_LINES; do
 		set_link=0
 		CALENTRY_LIST=(`for day in ${MONTH_LIST[@]}; do echo $day; done |grep $dn`)
@@ -56,6 +56,7 @@ for line in ${NUM_DAY_LINES[@]}; do
 			echo '<td style="text-align:center"><span class="calendar">'$dn'</span></td>' >> "$PLUGIN_OUTFILE"
 		fi
 	done
+	echo "$DN_LINES" | sed -e '/^  / Q; /  [ \t]/ s//<td style="text-align: center;"><\/td>\ /g; /[0-9]/ s///g' >> "$PLUGIN_OUTFILE"
 	echo '</tr>' >> "$PLUGIN_OUTFILE"
 done
 echo '</table>' >> "$PLUGIN_OUTFILE"
