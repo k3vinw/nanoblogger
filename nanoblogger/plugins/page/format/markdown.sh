51b6388f6a569589cd06c87e2fbb7a5da2066030
n1xt3r <4961438+k3vinw@users.noreply.github.com>
2008-06-13 22:12:47 +0000

    Created 3.4 Branch.

---
+++ nanoblogger/plugins/page/format/markdown.sh
@@ -0,0 +1,10 @@
+# NanoBlogger plugin to filter page content through markdown
+# Markdown is documented and implemented at
+#   <URL:http://daringfireball.net/projects/markdown/>
+
+MARKDOWN="/usr/bin/markdown"
+MARKDOWN_OPTS=""
+
+nb_msg "$plugins_entryfilteraction `basename $nb_plugin` ..."
+MKPAGE_CONTENT=`echo "$MKPAGE_CONTENT" | ${MARKDOWN} ${MARKDOWN_OPTS}`
+
