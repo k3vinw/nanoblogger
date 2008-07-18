0beb6ae4a32e5abca781aa15f8dad1aef00aeb1f
n1xt3r <n1xt3r@2a92b892-5743-0410-993c-ec182001eea1>
2008-07-18 16:34:58 +0000

    - Aborts when markdown plugin can't find command in path, so user is   informed about the problem and formatting isn't lost. - Adds new message for when aborting plugins.

--- branches/nanoblogger-3.4/nanoblogger/plugins/page/format/markdown.sh
+++ branches/nanoblogger-3.4/nanoblogger/plugins/page/format/markdown.sh
@@ -11,5 +11,7 @@ eval $MARKDOWN_CMD > "$SCRATCH_FILE".plugin_devnull 2>&1 &&
 if [ "$MARKDOWN_INPATH" = "true" ]; then
 	nb_msg "$plugins_entryfilteraction `basename $nb_plugin` ..."
 	MKPAGE_CONTENT=`echo "$MKPAGE_CONTENT" | ${MARKDOWN_CMD} ${MARKDOWN_OPTS}`
+else
+	die "$nb_plugin: $plugins_abort"
 fi
 
